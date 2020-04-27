0fbbc6348ff99de6c631adb291e93fe28518a297
gr-olga <gr-olga@ya.ru>
2020-04-27 16:15:41 +0200

    WIP: added history,

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import {Menu} from './Menu'
+
 import {Board} from "./Square";
 
 
 function App() {
-    const name = 'Sergei'
+
 
     return (
         <div className="App">
-            <Menu name={name}/>
-            <Board/>
+
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    My name is {name} <br/>
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <Board/>
             </header>
         </div>
     );
